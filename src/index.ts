43c2bb234e5aa9d0d0be525a4aff467d271fcb00
Tiago Santos <trodrigues322@gmail.com>
2022-03-10 15:33:42 -0300

    chore(standardizing commit messages): add commitzen and lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
-import express from "express";
-import "reflect-metadata";
+import 'dotenv/config';
+import express from 'express';
+import 'reflect-metadata';
 import './database/conect';
 import routes from './routes';
 
@@ -9,4 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
+app.listen(3333, () =>
+  console.log('🔥 Server started at http://localhost:3333'),
+);
