04283ee37bd6b28e0ac699b719ff67734f88ef6f
Tiago Santos <trodrigues322@gmail.com>
2022-03-10 14:28:09 -0300

    chore: config typeorm

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
+
+module.exports = {
+    type: "postgres",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    entities: [
+        "src/app/models/*.ts"
+    ],
+    migrations: [
+        "src/database/migrations/*.ts"
+    ],
+    cli: {
+        "migrationsDir": "src/database/migrations/*.ts"
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import express from "express";
+import "reflect-metadata";
+import './database/conect';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
