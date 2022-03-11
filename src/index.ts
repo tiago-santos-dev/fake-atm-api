839e1d913686b0c9ffc722c792802d0fc857ff2e
Tiago Santos <trodrigues322@gmail.com>
2022-03-11 09:46:05 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import './database/conect';
 import routes from './routes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333, () =>
